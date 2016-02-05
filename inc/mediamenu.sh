fe328277a34fa22eaa9cc11f724983ff6a1845ce
smx <smxsmx4@gmail.com>
2016-02-05 05:10:19 +0100

    Fixed mediamenu recursive calls. Rewritten file chooser menus

--- inc/mediamenu.sh
+++ inc/mediamenu.sh
@@ -8,16 +8,15 @@ function mediamenu(){
 				err_exit "Can't map ${in_arg}\n"
 			fi
 			dev_target="/dev/nbd0"
+      if [ $virtualdev -eq 1 ]; then
+        dev_target="${dev_target}p1"
+      else
+        dev_target="${dev_target}1"
+      fi
 		fi
 		if [ ! -b "/dev/nbd0p1" ]; then
 			err_exit "Corrupted image\n"
 		fi
-	fi
-
-	if [ $virtualdev -eq 1 ]; then
-		dev_target="${dev_target}p1"
-	else
-		dev_target="${dev_target}1"
 	fi
 
 	if ! grep -q "/mnt/osx/target" /proc/mounts; then
@@ -130,188 +129,103 @@ function mediamenu(){
 	esac
 }
 
-function tweakmenu(){
-	local tweaks=($(find "$scriptdir/tweaks" -maxdepth 1 -type f -name "*.sh"))
-	if [ ${#tweaks[@]} -eq 0 ]; then
-		$lred; echo "No tweak to install"; $normal
-		pause "Press [enter] to return to menu"
-		mediamenu
-	fi
-	printf "Choose a tweak to apply: "
-	$white; echo "0 - Return to main menu"; $normal
+function fileChooser(){
+  local dir="$1"
+  local ext="$2"
+  local type="$3"
+  local dirtype="$(basename ${dir})"
 
-	for i in ${!tweaks[@]}; do
-		local name=$(grep tweakname= ${tweaks[$i]} | grep -o "=.*" | sed 's|[="]||g')
-		echo $((i + 1)) - ${name}
-	done
-	$white; echo "Choose a tweak to apply"; $normal
-	read choice
-	if [ -z $choice ] || [ $choice -lt 0 ] || [ $choice -gt ${#tweaks[@]} ]; then
-		pause "Invalid option, press [enter] to try again"
-		clear
-		tweakmenu
-	elif [ $choice -eq 0 ]; then
-		clear
+  local files=($(find "${dir}" -maxdepth 1 -type ${type} -not -name .gitignore -name "${ext}"))
+  if [ ${#files[@]} -eq 0 ]; then
+    $lred; echo "No option available"; $normal
+    pause "Press [enter] to return to menu"
 		mediamenu
-	else
+  fi
+
+  $white; echo "0 - Return to main menu"; $normal
+
+  local install_dir
+
+  case $dir in
+    */kernels)
+      install_dir="/mnt/osx/target";
+      ;;
+    */extra_kexts)
+      install_dir="/mnt/osx/target/Extra/Extensions"
+      ;;
+    */chameleon/Modules)
+      install_dir="/mnt/osx/target/Extra/Extensions"
+      ;;
+  esac
+
+  for i in ${!files[@]}; do
+    case $dir in
+      */tweaks)
+        local name=$(grep tweakname= ${files[$i]} | grep -o "=.*" | sed 's|[="]||g')
+        echo $((i + 1)) - ${name}
+        ;;
+      *)
+        local bname="$(basename ${files[$i]})"
+        if [ -${type} "${install_dir}/${bname}" ]; then
+          printf "[*]\t$((i + 1)) - ${bname}\n"
+        else
+          printf "[ ]\t$((i + 1)) - ${bname}\n"
+        fi
+        ;;
+    esac
+  done
+
+  $white; printf "Choose an option: "; $normal
+	read choice
+  if [ -z $choice ] || [ $choice -lt 0 ] || [ $choice -gt ${#files[@]} ]; then
+    pause "Invalid option, press [enter] to try again"
 		clear
-		local tweak=${tweaks[$((choice-1))]}
-		$yellow; echo "Applying ${tweak}..."; $normal
-		bash "${tweak}"
-	fi
-	$lgreen; echo "Done!"; $normal
-	tweakmenu
+    fileChooser
+  elif [ $choice -eq 0 ]; then
+    clear
+    mediamenu
+  else
+    local option=${files[$((choice-1))]}
+    case $dirtype in
+      */tweaks)
+        $yellow; echo "Applying ${option}..."; $normal
+        bash "${option}"
+        ;;
+      *)
+        local bname="$(basename ${option})"
+        if [ ! -d "${install_dir}" ]; then
+          mkdir -p "${install_dir}"
+        fi
+        if [ -${type} "${install_dir}/${bname}" ]; then
+    			$yellow; echo "Removing ${bname}..."; $normal
+    			rm -R "${install_dir}/${bname}"
+    		else
+    			$yellow; echo "Installing ${bname}..."; $normal
+    			cp -R "${option}" "${install_dir}"
+    			chown -R 0:0 "${install_dir}/${bname}"
+    			chmod -R 755 "${install_dir}/${bname}"
+    		fi
+        ;;
+    esac
+  fi
+  $lgreen; echo "Done!"; $normal
+  pause
+  clear
+  fileChooser "${dir}" "${ext}" "${type}"
+}
+
+function tweakmenu(){
+  fileChooser "${scriptdir}/tweaks" "*.sh" "f"
 }
 
 function kextmenu(){
-	kexts=$(find "$kextdir" -maxdepth 1 -type d -name "*.kext" | wc -l)
-	if [ $kexts == 0 ]; then
-		$lred; echo "No kext to install"; $normal
-		pause "Press [enter] to return to menu"
-		mediamenu
-	fi
-	printf "Choose a kext to Install / Reinstall: "
-	local k
-	local eskdir=$(echo "$kextdir" | sed 's/\ /\\\//g;s/\//\\\//g')
-	$white; echo "0 - Return to main menu"; $normal
-	for k in `seq $kexts`; do
-		local option=$(find "$kextdir" -maxdepth 1 -type d -not -name ".gitignore" -name "*.kext" | sed "s/$eskdir\///g" | sed -n "$k"p)
-			eval kext$k="$option"
-			if [ -d "/mnt/osx/target/Extra/Extensions/"$option"" ]; then
-				printf "[*]\t$k - $option\n"
-			else
-				printf "[ ]\t$k - $option\n"
-			fi
-	done
-	$white; echo "Choose a kext to install/uninstall"; $normal
-	read choice
-	local name="kext$choice"
-	if [ "$choice" == "0" ]; then
-		clear
-		mediamenu
-	fi
-	if [ -z "${!name}" ]; then
-		pause "Invalid option, press [enter] to try again"
-		clear
-		kextmenu
-	else
-	clear
-		if [ -d "/mnt/osx/target/Extra/Extensions/${!name}" ]; then
-			$yellow; echo "Removing ${!name}..."; $normal
-			rm -R "/mnt/osx/target/Extra/Extensions/${!name}"
-		else
-			$yellow; echo "Installing ${!name}..."; $normal
-			cp -R "$kextdir/${!name}" /mnt/osx/target/Extra/Extensions/
-			chown -R 0:0 "/mnt/osx/target/Extra/Extensions/${!name}"
-			chmod -R 755 "/mnt/osx/target/Extra/Extensions/${!name}"
-		fi
-	fi
-	$lgreen; echo "Done!"; $normal
-	kextmenu
+  fileChooser "${scriptdir}/extra_kexts" "*.kext" "d"
 }
 
 function chammodmenu(){
-	modules=$(find "$scriptdir/chameleon/Modules" -maxdepth 1 -type f -name "*.dylib" | wc -l)
-	if [ $modules == 0 ]; then
-		$lred; echo "No module to install"; $normal
-		pause "Press [enter] to return to menu"
-		mediamenu
-	fi
-	printf "Choose a module to Install / Reinstall: "
-	local m
-	local esmdir=$(echo "$scriptdir/chameleon/Modules" | sed 's/\ /\\\//g;s/\//\\\//g')
-	$white; echo "0 - Return to main menu"; $normal
-	for m in `seq $modules`; do
-		local option=$(find "$scriptdir/chameleon/Modules" -maxdepth 1 -type f -not -name ".gitignore" -name "*.dylib" | sed "s/$esmdir\///g" | sed -n "$m"p)
-			eval module$m="$option"
-			if [ -f "/mnt/osx/target/Extra/Modules/"$option"" ]; then
-				printf "[*]\t$m - $option\n"
-			else
-				printf "[ ]\t$m - $option\n"
-			fi
-	done
-	$white; echo "Choose a module to install/uninstall"; $normal
-	read choice
-	local name="module$choice"
-	if [ "$choice" == "0" ]; then
-		clear
-		mediamenu
-	fi
-	if [ -z "${!name}" ]; then
-		pause "Invalid option, press [enter] to try again"
-		clear
-		chammodmenu
-	else
-	clear
-		if [ -f "/mnt/osx/target/Extra/Modules/${!name}" ]; then
-			$yellow; echo "Removing ${!name}..."; $normal
-			rm "/mnt/osx/target/Extra/Modules/${!name}"
-		else
-			$yellow; echo "Installing ${!name}..."; $normal
-			cp "$scriptdir/chameleon/Modules/${!name}" /mnt/osx/target/Extra/Modules/
-			chmod -R 755 "/mnt/osx/target/Extra/Modules/${!name}"
-		fi
-	fi
-	$lgreen; echo "Done!"; $normal
-	chammodmenu
+  fileChooser "${scriptdir}/chameleon/Modules" "*.dylib" "f"
 }
 
 function kernelmenu(){
-	kernels=$(find "$kerndir" -maxdepth 1 -type f -not -name ".gitignore" | wc -l)
-	if [ $kernels == 0 ]; then
-		$lred; echo "No kernel to install"; $normal
-		pause "Press [enter] to return to menu"
-		mediamenu
-	fi
-	printf "Choose a kernel to Install / Reinstall: "
-	local k
-	local eskdir=$(echo "$kerndir" | sed 's/\ /\\\//g;s/\//\\\//g')
-	$white; echo "0 - Return to main menu"; $normal
-	for k in `seq $kernels`; do
-		local option=$(find "$kerndir" -maxdepth 1 -type f -not -name ".gitignore" | sed "s/$eskdir\///g" | sed -n "$k"p)
-			eval kern$k="$option"
-			if [ -f "/mnt/osx/target/"$option"" ]; then
-				printf "[*]\t$k - $option\n"
-			else
-				printf "[ ]\t$k - $option\n"
-			fi
-	done
-	$white; echo "Choose a kernel to install/uninstall"; $normal
-	read choice
-	local name="kern$choice"
-	if [ "$choice" == "0" ]; then
-		clear
-		mediamenu
-	fi
-	if [ -z "${!name}" ]; then
-		pause "Invalid option, press [enter] to try again"
-		clear
-		kernelmenu
-	else
-	clear
-		if [ -f "/mnt/osx/target/${!name}" ]; then
-			if [ "${!name}" == "mach_kernel" ]; then #stock kernel
-				read -p "Warning, you are about to overwrite the default Kernel. Do you want to back it up to \"apple_kernel\"? (yes/no/abort)" -n1 -r
-				echo
-				if [[ $REPLY =~ ^[Aa]$ ]];then
-					kernelmenu
-				elif [[ $REPLY =~ ^[Yy]$ ]];then
-					$yellow; echo "Backing up mach_kernel..."; $normal
-					mv /mnt/osx/target/mach_kernel /mnt/osx/target/apple_kernel
-					$yellow; echo "Copying new mach_kernel..."; $normal
-					cp $verbose "$kerndir/${!name}" /mnt/osx/target/
-					chmod 755 "/mnt/osx/target/${!name}"
-				fi
-			else #alternative kernel name, we can delete
-				$yellow; echo "Removing ${!name}..."; $normal
-				rm $verbose "/mnt/osx/target/${!name}"
-			fi
-		else
-			$yellow; echo "Installing ${!name}..."; $normal
-			cp $verbose "$kerndir/${!name}" /mnt/osx/target/
-			chmod 755 "/mnt/osx/target/${!name}"
-		fi
-	fi
-	$lgreen; echo "Done!"; $normal
-	kernelmenu
+  fileChooser "${scriptdir}/kernels" "*" "f"
 }
