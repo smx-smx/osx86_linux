fb2a8a107e35236e529316e70aab480d82f3ab7d
smx <smxsmx4@gmail.com>
2016-03-06 22:57:50 +0100

    Fixed dialog based copy progress. Rewritten do_kernel to avoid extracting pkg files when possible

--- inc/osx_base.sh
+++ inc/osx_base.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
+function is_splitted(){
+	[ -f "/mnt/osx/esd/BaseSystem.dmg" ] && return 0
+	return 1
+}
+
 function detect_osx_version(){
+	# Path to the SystemVersion plist
 	local verfile
+	# MountPoint where the version file is located
 	local mountpoint
 
-	if [ $mediamenu -eq 1 ]; then #look in target
+	# mediamenu -> target
+	if [ $mediamenu -eq 1 ]; then
 		mountpoint="target"
 		$lyellow; echo "Scanning OSX version on $dev_target...";$normal
-	elif [ -f "/mnt/osx/esd/BaseSystem.dmg" ]; then
+	# splitted -> basesystem
+	elif is_splitted; then
 		mountpoint="base"
 		$lyellow; echo "Scanning OSX version on BaseSystem..."; $normal
-	else #look in installer
+	# dmg/unified
+	else
 		mountpoint="esd"
 		$lyellow; echo "Scanning OSX version on DMG..."; $normal
 	fi
@@ -58,15 +68,17 @@ function do_preptarget(){
 		if [ $vbhdd -eq 0 ]; then #raw image
 			$yellow; echo "Creating Image..."; $normal
 			if [ -f "$dev_target" ]; then
-				$lred; read -p "Image $dev_target already exists. Overwrite? (y/n)" -n1 -r
-				echo; $normal
-				if [[ $REPLY =~ ^[Nn]$ ]];then
+				$lred
+				if ! read_yn "Image $dev_target already exists. Overwrite?"; then
 					err_exit ""
 				fi
+				$normal
 			fi
+
 			if ! dd if=/dev/zero bs=1 of="$dev_target" seek="$size" count=0; then
 				err_exit "Error during image creation\n"
 			fi
+
 			if [ ! $nbd0_mapped -eq 1 ]; then
 				echo "Mapping virtual dev with qemu..."
 				if ! qemu_map "nbd0" "$dev_target"; then
@@ -205,10 +217,40 @@ function do_preptarget(){
 	fi
 }
 
+function copy_progress(){
+	local desc="$1"
+	local rsync_options="$2"
+	local rsync_source="$3"
+	local rsync_dest="$4"
+
+	local rsync_size
+	$white; echo "Calculating ${desc} size..."; $normal
+	rsync_size=$(du -b --apparent-size -sc ${rsync_source}/* | tail -n1 | awk '{print $1}')
+	$white; echo "Copying ${desc} to ${rsync_dest}..."; $normal
+
+
+	dialog --title "osx86_linux" --gauge "Copying ${desc}..." 10 75 < <(
+		local lines=0
+		rsync ${rsync_flags} ${rsync_source} ${rsync_dest} | unbuffer -p awk '{print $1}' | while read -r line; do
+			# Don't count empty lines
+			if [ ! -z "$line" ]; then
+				lines=$(($lines + 1))
+				# If rsync was refreshed enough times, update progress
+				if [ $lines -gt $DIALOG_THRES ]; then
+					# Remove nasty characters (trim), and remove the numeric separator
+					local doneSz=$(trim $(echo $line | sed 's/,//g'))
+					echo $(($doneSz * 100 / $rsync_size))
+					lines=0
+				fi
+			fi
+		done
+	)
+}
+
 function do_system(){
 	local rsync_flags=""
 	local rsync_source
-	if [ -f "/mnt/osx/esd/BaseSystem.dmg" ]; then
+	if is_splitted; then
 		rsync_source="/mnt/osx/base/"
 	else
 		rsync_source="/mnt/osx/esd/"
@@ -222,21 +264,7 @@ function do_system(){
 		rsync ${rsync_flags} ${rsync_source} /mnt/osx/target/
 	else
 		rsync_flags="-ar ${verbose} --info=progress2"
-		local rsync_size
-		$white; echo "Calculating Base System size..."; $normal
-		rsync_size=$(du -B1 -sc ${rsync_source}/* | tail -n1 | awk '{print $1}')
-		$white; echo "Copying Base System to ${dev_target}..."; $normal
-		dialog --title "osx86_linux" --gauge "Copying base system..." 10 75 < <(
-			local lines=0
-			rsync ${rsync_flags} ${rsync_source} /mnt/osx/target/ | unbuffer -p awk '{print $1}' | while read line; do
-				lines=$(($lines + 1))
-				if [ $lines -gt $DIALOG_THRES ]; then
-					local doneSz=$(trim $(echo $line | sed 's/,//g'))
-					echo $((doneSz * 100 / rsync_size))
-					lines=0
-				fi
-			done
-		)
+		copy_progress "Base System" "${rsync_flags}" "${rsync_source}" "/mnt/osx/target/"
 	fi
 
 	if [ -d "/mnt/osx/esd/Packages" ]; then
@@ -244,53 +272,156 @@ function do_system(){
 		mkdir $verbose /mnt/osx/target/System/Installation/Packages
 
 		rsync_source="/mnt/osx/esd/Packages/"
+
 		if [ $log_mode -eq 1 ]; then
 			$white; echo "Copying installation packages..."; $normal
 			rsync ${rsync_flags} ${rsync_source} /mnt/osx/target/System/Installation/Packages/
 		else
-			$white; echo "Calculating Installation Packages size..."; $normal
-			rsync_size=$(du -B1 -sc ${rsync_source}/* | tail -n1 | awk '{print $1}')
-			dialog --title "osx86_linux" --gauge "Copying installation packages..." 10 75 < <(
-				local lines=0
-				rsync ${rsync_flags} ${rsync_source} /mnt/osx/target/System/Installation/Packages/ | unbuffer -p awk '{print $1}' | while read line; do
-					local lines=$(($lines + 1))
-					if [ $lines -gt $DIALOG_THRES ]; then
-						local doneSz=$(trim $(echo $line | sed 's/,//g'))
-						echo $((doneSz * 100 / rsync_size))
-						lines=0
-					fi
-				done
-			)
+			copy_progress "Installation Packages" "${rsync_flags}" "${rsync_source}" "/mnt/osx/target/System/Installation/Packages/"
 		fi
 		sync
 	fi
 }
 
 function do_kernel(){
-	# Source of kernel files
-	mountpoint=$1
-	$yellow; echo "Copying kernel..."; $normal
-	local kernel_cache_path="System/Library/Caches/com.apple.kext.caches/Startup/kernelcache"
-	local osver_minor=$(echo $osver | cut -d '.' -f2)
-	# Mavericks and above
-	if [ $osver_minor -ge 9 ]; then
-		$lyellow; echo "Kernel is in BaseSystemBinaries.pkg, extracting..."; $normal
-		local esd_path
-		if [ ! "$mountpoint" == "esd" ]; then
-			target_path="System/Installation"
-		fi
-		extract_pkg "/mnt/osx/${mountpoint}/${target_path}/Packages/BaseSystemBinaries.pkg" "${scriptdir}/tmp/bsb" "skip"
-		if [ $osver_minor -eq 9 ]; then
-			cp -a $verbose "${scriptdir}/tmp/bsb/mach_kernel" "/mnt/osx/target/"
-		else
-			cp -a $verbose "${scriptdir}/tmp/bsb/${kernel_cache_path}" "/mnt/osx/target/${kernel_cache_path}"
-		fi
-	# This won't work from mediamenu and < 10.10 (esd not mounted there)
-	elif [ -f "/mnt/osx/esd/mach_kernel" ]; then
-		cp -av /mnt/osx/esd/mach_kernel /mnt/osx/target/
+	local nkernels
+	local kernel
+	local path
+
+	local result=1 #error
+	local prefix="/mnt/osx/target"
+
+	local paths="target"
+	if [ $mediamenu -eq 0 ]; then
+		paths="esd base ${paths}"
 	fi
-	if [ ! -f "/mnt/osx/target/mach_kernel" ] && [ ! -f "/mnt/osx/target/${kernel_cache_path}" ]; then
-		$lred; echo "WARNING! Kernel installation failed!!"
-		echo "Media will likely be unbootable!"; $normal
+
+
+	local path_pre
+	local path_post
+
+	for mountpoint in $paths; do
+		$lyellow; echo "Searching for kernels in ${mountpoint}..."; $normal
+
+		$white; echo "Searching for prelinkedkernels..."; $normal
+		path_pre="/mnt/osx/${mountpoint}"
+		path_post="/System/Library/PrelinkedKernels"
+
+		path="${path_pre}/${path_post}"
+		if [ -d "${path}" ]; then
+			# How many kernels are there?
+			nkernels=$(ls -1 ${path} | wc -l)
+			if [ $nkernels -gt 0 ]; then
+				# Pick the first one, should be "prelinkedkernel"
+				kernel=$(ls -1 ${path} | head -n1)
+				if [ -f "${path}/${kernel}" ] && [ ! -f "${prefix}/${path_post}/${kernel}" ]; then
+					$lgreen; echo "Found prelinkedkernel in ${mountpoint}"; $normal
+					cp -ar ${verbose} "${path}/${kernel}" "${prefix}/${path_post}/${kernel}"
+					result=0
+					break
+				fi
+			fi
+		fi
+
+		$white; echo "Searching for kernelcaches..."; $normal
+		path_post="/System/Library/Caches/com.apple.kext.caches/Startup"
+
+		path="${path_pre}/${path_post}"
+		if [ -d "${path}" ]; then
+			nkernels=$(ls -1 ${path} | wc -l)	
+			if [ $nkernels -gt 0 ]; then
+				# Pick the first one, should be "kernelcache"
+				kernel=$(ls -1 ${path} | head -n1)
+				if [ -f "${path}/${kernel}" ] && [ ! -f "${prefix}/${path_post}/${kernel}" ]; then
+					$lgreen; echo "Found kernelcache in ${mountpoint}"; $normal
+					cp -ar ${verbose} "${path}/${kernel}" "${prefix}/${path_post}/${kernel}"
+					result=0
+					break
+				fi
+			fi
+		fi
+
+		$white; echo "Searching for mach_kernel..."; $normal
+		path_post="/mach_kernel"
+
+		path="${path_pre}/${path_post}"
+		if [ -f "${path}" ] && [ ! -f "${prefix}/${path_post}" ]; then
+			$lgreen; echo "Found mach_kernel in ${mountpoint}"; $normal
+			cp -ar ${verbose} "${path}" "${prefix}/${path_post}"
+			result=0
+			break
+		fi
+
+	done
+
+	$white; echo "Searching in installation packages..."; $normal
+
+	# Attempt pkg extraction
+	if [ ! $result -eq 0 ]; then
+		local pkg_file
+		local kernel_path
+		local osver_minor=$(echo $osver | cut -d '.' -f2)
+
+		case $osver_minor in
+			# TODO: Not sure about 10
+			9-10)
+				pkg_file="BaseSystemBinaries.pkg"
+				# Relative to the package
+				kernel_path="/mach_kernel"
+				;;
+			11)
+				pkg_file="Essentials.pkg"
+				# Relative to the package
+				kernel_path="/System/Library/Kernels/kernel"
+				;;
+			*)
+				;;
+		esac
+
+		if [ ! -z "${pkg_file}" ]; then
+			$lyellow; echo "Kernel found in ${pkg_file}, extracting..."; $normal
+
+			local target_path
+			local mountpoint
+			# For non splitted images (10.6 DVD for instance)
+			if ! is_splitted || [ $mediamenu -eq 1 ]; then
+				target_path="/System/Installation"
+			fi
+
+			# The system installation packages are in ESD in all cases.
+			# The only exception is when we are in mediamenu
+			# We have no ESD or BaseSystem mounted, but we may have the installation packages
+			# Unless the drive is missing OS X setup files
+			if [ $mediamenu -eq 1 ]; then
+				mountpoint="target"
+			else
+				mountpoint="esd"
+			fi
+
+			# basename -> full path
+			pkg_file="/mnt/osx/${mountpoint}/${target_path}/Packages/${pkg_file}"
+			if [ ! -f "${pkg_file}" ]; then
+				err_exit "Cannot continue, missing package ${pkg_file}\n"
+			fi
+
+			# Extract to tmp without the extension
+			local dst_path="${scriptdir}/tmp/$(basename ${pkg_file%.*})"
+			# TODO: Fix CPIO filter
+			#if ! extract_pkg "${pkg_file}" "${dst_path}" "${kernel_path}"; then
+			if ! extract_pkg "${pkg_file}" "${dst_path}" ""; then
+				err_exit "${pkg_file} exraction FAILED!\n"
+			fi
+
+			# Extracted kernel -> target
+			cp -a $verbose "${dst_path}/${kernel_path}" "/mnt/osx/target/mach_kernel"
+		fi
 	fi
+
+	if [ ! $result -eq 0 ]; then
+		$lred
+		echo "Couldn't find a suitable kernel!"
+		echo "Installation WILL NOT BE BOOTABLE"
+		$normal
+	fi
+	return $result	
 }
